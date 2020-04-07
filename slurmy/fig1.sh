34396e53ff24bd1e2672fd24ed4d637267e9f766
Josh Popp <joshpopp@Joshs-MacBook-Pro-9.local>
2020-04-07 00:50:06 -0400

    set partition for slurm

--- slurmy/fig1.sh
+++ slurmy/fig1.sh
@@ -4,6 +4,7 @@
 #SBATCH --output=slurm_output/fig1.out
 #SBATCH --error=slurm_output/fig1.err
 #SBATCH --mem=40G
+#SBATCH --partition=shared
 #SBATCH --mail-user=jpopp4@jhu.edu
 
 module load python/3.7-anaconda
