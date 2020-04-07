34b9fc94c9580708587d858e24739e9a3667dc26
Josh Popp <joshpopp@Joshs-MacBook-Pro-9.local>
2020-04-07 15:22:54 -0400

    fixed directory naming system

---
+++ fig1.py
@@ -0,0 +1,43 @@
+#!/bin/python
+import numpy as np
+import pandas as pd
+import scanpy as sc
+import anndata
+import limix
+
+COUNTS_LOC = "/work-zfs/abattle4/lab_data/sc_endo_diff/counts.tsv"
+METADATA_LOC = "/work-zfs/abattle4/lab_data/sc_endo_diff/cell_metadata_cols.tsv"
+GENENAMES_LOC = "/home-2/jpopp4@jhu.edu/work/josh/sc-endoderm-differentiation/data/genenames.txt"
+
+def load_data():
+    counts = np.loadtxt(COUNTS_LOC, delimiter="\t", skiprows=1, usecols=range(1, 36045))
+    counts = np.transpose(counts)
+    metadata = pd.read_csv(METADATA_LOC, sep="\t")
+    gene_names = pd.read_csv(GENENAMES_LOC, sep=' ', header=0)
+    adata = anndata.AnnData(X=counts, obs=metadata)
+    adata.var_names = gene_names.keys()
+    return adata
+
+def var_genes(adata):
+    sc.pp.highly_variable_genes(adata, n_top_genes=500, batch_key='experiment')
+    sc.pl.highly_variable_genes(adata, save=".png")
+    return adata
+
+def dim_reduce(adata):
+    sc.pp.scale(adata)
+    sc.tl.pca(adata, svd_solver='arpack')
+    pve1 = adata.uns['pca']['variance_ratio'][0]
+    pve2 = adata.uns['pca']['variance_ratio'][1]
+    sc.pl.pca(adata, color='day', save=".png")
+    return adata
+
+def main():
+    results_file="./data/sc.endo.h5ad"
+    adata = load_data()
+    adata = var_genes(adata)
+    adata = dim_reduce(adata)
+    adata.write(results_file)
+
+
+if __name__=="__main__":
+    main()
--- slurmy/fig1.sh
+++ slurmy/fig1.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 #SBATCH --job-name=fig1
 #SBATCH --time=30:0
-#SBATCH --output=slurm_output/fig1.out
-#SBATCH --error=slurm_output/fig1.err
+#SBATCH --output=log/fig1.out
+#SBATCH --error=log/fig1.err
 #SBATCH --mem=40G
 #SBATCH --partition=shared
-#SBATCH --mail-user=jpopp4@jhu.edu
 
 module load python/3.7-anaconda
 
