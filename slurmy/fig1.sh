506c01e39eae0720959fafa0433deb7f6696efcf
Josh Popp <joshpopp@Joshs-MacBook-Pro-9.local>
2020-04-07 00:49:04 -0400

    slurmy

---
+++ slurmy/fig1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --job-name=fig1
+#SBATCH --time=1:0:0
+#SBATCH --output=slurm_output/fig1.out
+#SBATCH --error=slurm_output/fig1.err
+#SBATCH --mem=40G
+#SBATCH --mail-user=jpopp4@jhu.edu
+
+module load python/3.7-anaconda
+
+python fig1.py
